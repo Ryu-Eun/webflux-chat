78d78aaf3397d3f241a83acebef22f89fe8c6520
Ryu-Eun <ektl4m@gmail.com>
2025-08-30 08:55:12 +0900

    [feature] V2 Flyway.sql 파일에 blocklist 테이블 생성 쿼리 추가 - 친구관계(양방향)과 차단관계(단방향)의 분리

--- src/main/resources/db/migration/V2__friend_request_and_friendship_and_block_list.sql
+++ src/main/resources/db/migration/V2__friend_request_and_friendship_and_block_list.sql
@@ -19,3 +19,11 @@ CREATE TABLE IF NOT EXISTS friendships (
     version BIGINT NOT NULL DEFAULT 0,
     CONSTRAINT uq_friendship UNIQUE (user_id, friend_user_id)
 );
+
+CREATE TABLE IF NOT EXISTS block_list (
+    id BIGSERIAL PRIMARY KEY,
+    user_id BIGINT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    blocked_user_id BIGINT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+    CONSTRAINT uq_block UNIQUE (user_id, blocked_user_id)
+);
