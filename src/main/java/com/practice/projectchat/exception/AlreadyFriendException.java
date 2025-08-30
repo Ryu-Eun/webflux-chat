94a96b0fc28d1cf59ea832c4b7810201ff36860f
Ryu-Eun <ektl4m@gmail.com>
2025-08-30 10:28:07 +0900

    [feature] CustomException 생성 - AlreadyFriendException - DuplicateFriendRequestException

---
+++ src/main/java/com/practice/projectchat/exception/AlreadyFriendException.java
@@ -0,0 +1,7 @@
+package com.practice.projectchat.exception;
+
+public class AlreadyFriendException extends RuntimeException {
+    public AlreadyFriendException(String message) {
+        super(message);
+    }
+}
