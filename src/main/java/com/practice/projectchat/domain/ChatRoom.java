67449f2818722d6c9e9c5e2533b4f329671dfa09
Ryu-Eun <ektl4m@gmail.com>
2025-09-07 11:30:16 +0900

    [comment] chatRoom관련 TODO 주석 몇개 추가 - 나중에 동시성문제 해결 - 나중에 chatRoom 이름 관련 확장하려면 아예 다른 테이블로 분리 필요

--- src/main/java/com/practice/projectchat/domain/ChatRoom.java
+++ src/main/java/com/practice/projectchat/domain/ChatRoom.java
@@ -1,7 +1,6 @@
 package com.practice.projectchat.domain;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.LastModifiedDate;
@@ -11,8 +10,10 @@ import org.springframework.data.relational.core.mapping.Table;
 
 import java.time.Instant;
 
+
+// TODO: A와 B가 동시에 PRIVATE 채팅방을 만들면 두개가 생길 수 있음.(동시성 문제) -> 나중에 해결
 @Getter
-@Setter
+@Builder
 @Table(name = "chat_rooms")
 public class ChatRoom {
 
@@ -23,7 +24,8 @@ public class ChatRoom {
     private Long id;
 
     // 1:1일 경우 방이름은 상대방 닉네임, Group일 경우 상대방 닉네임 나열
-    // 1:1은 name 컬럼에 저장하지 않고, 상대방 닉네임으로 동적 표시. Group은 name 컬럼을 두고 저장, 이후에 수정 가능
+    // 1:1은 name 컬럼에 저장하지 않고, 상대방 닉네임으로 동적 표시. Group은 name컬럼에 해당 구성원들의 이름들 나열로 들어감.
+    // TODO: 각자 바라보는 대화창의 이름을 다르게 해주려면 테이블을 하나 더 생성해야하는데 그럼 초기개발에 너무 복잡해지니까 나중으로 미루기, 그리고 만약 대화방을 처음 만든 사용자가 대화방이름 변경 권한을 가지게 할수도 있는데, 그것도 복잡해질거같아서 넘김
     @Column("name")
     private String name;
 
