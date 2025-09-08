c5fc8c1cac9f6eb805d15845b8efeb76f58fa617
Ryu-Eun <ektl4m@gmail.com>
2025-09-08 12:30:56 +0900

    [feature] ChatRoomMember에 @Setter,@Builder 필요한 부분 추가

--- src/main/java/com/practice/projectchat/domain/ChatRoomMember.java
+++ src/main/java/com/practice/projectchat/domain/ChatRoomMember.java
@@ -1,5 +1,6 @@
 package com.practice.projectchat.domain;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
@@ -10,7 +11,7 @@ import org.springframework.data.relational.core.mapping.Table;
 import java.time.Instant;
 
 @Getter
-@Setter
+@Builder
 @Table(name = "chat_room_members")
 public class ChatRoomMember {
 
@@ -26,12 +27,15 @@ public class ChatRoomMember {
 
     // 방에 나갔다고 row를 삭제시키지않고, 히스토리를 활용할 수도 있기 때문에 isActive 컬럼으로 true/false 시킨다
     @Column("is_active")
+    @Setter
     private Boolean isActive;
 
     @Column("joined_at")
+    @Setter
     private Instant joinedAt;
 
     @Column("left_at")
+    @Setter
     private Instant leftAt; // 초기값 null
 
     @Version
